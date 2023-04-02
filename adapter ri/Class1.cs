734671f031057ccbc77783e9bd5fbce5d2b8a8bb
олег <@DESKTOP-SSTKF2A>
2023-04-02 12:33:33 +0300

    Добавьте файлы проекта.

---
+++ adapter ri/Class1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace adapter_ri
+{
+    
+        public interface ITransformer
+        {
+            void Move();
+            void Transform();
+            void Draw();
+            void AsphaltSpeed();
+        void DirtSpeed();
+
+        }
+        
+
+        public class AsphaltRoad
+        {
+
+            
+            public void Drive(ITransformer transformer)
+            {
+                transformer.Move();
+            }
+                
+            public void Show()
+            {
+
+                MessageBox.Show("car is moving on asphalt"  );
+            }
+        }
+
+        public class DirtRoad
+        {
+
+            public void Drive(ITransformer transformer)
+            {
+                transformer.Move();
+
+            }
+            
+            public void Show()
+            {
+
+                MessageBox.Show("car is moving on dirt"  );
+                
+            }
+        }
+        public class TransformerAdapter : ITransformer
+        {
+            private AsphaltRoad asphaltRoad;
+            private DirtRoad dirtRoad;
+
+
+            private bool isAsphalt;
+
+            public TransformerAdapter()
+            {
+                asphaltRoad = new AsphaltRoad();
+                dirtRoad = new DirtRoad();
+
+                isAsphalt = true;
+            }
+
+            public void Draw()
+            {
+                throw new NotImplementedException();
+            }
+            public void AsphaltSpeed()
+            {
+            int speed = 120;
+                
+                    MessageBox.Show("speed is " +speed);
+                
+                
+                
+                  
+                
+            }
+        public void DirtSpeed()
+        {int speed = 50;
+            MessageBox.Show("Speed is " + speed);
+        }
+
+            public void Move()
+            {
+                if (isAsphalt)
+                {
+                    asphaltRoad.Drive(this);
+                }
+                else
+                {
+                    dirtRoad.Drive(this);
+                }
+
+
+            }
+
+            public void Transform()
+            {
+                MessageBox.Show("Changingroad");
+
+            }
+        }
+
+    }
+
---
+++ adapter ri/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace adapter_ri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DirtRoad dirtRoad = new DirtRoad();
+            dirtRoad.Show();
+            TransformerAdapter Asphaltadapter = new TransformerAdapter();
+            Asphaltadapter.DirtSpeed();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            AsphaltRoad asphalt = new AsphaltRoad();
+            asphalt.Show();
+            TransformerAdapter Dirtadapter = new TransformerAdapter();
+            Dirtadapter.AsphaltSpeed();
+
+            
+        }
+    }
+}
